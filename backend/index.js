fe843518031e59c52f7d72769274a0bf161848eb
waymarlun <martin.lundin@vikingline.com>
2024-02-09 15:15:08 +0100

    update test endpoints

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,22 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-
+// POST endpoint
 app.post('/test-api', (req, res) => {
-  // Todo remove test-api endpoint before release
   console.log(req.body)
   res.status(200).json({ result: req.body.text });
+})
+
+// GET endpoint with no parameters
+app.get('/test-api', (req, res) => {
+  console.log(req.query)
+  res.status(200).json({ result: req.query });
+})
+
+// GET endpoint with path parameter
+app.get('/test-api/:input', (req, res) => {
+  console.log(req.params)
+  res.status(200).json({ result: req.params.input });
 })
 
 app.listen(port, () => {
